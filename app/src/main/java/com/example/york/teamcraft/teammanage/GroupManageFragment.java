be74f69a9f924c390b5863d65b98827232289ad9
York <kmgs4524@gmail.com>
2017-09-03 17:12:56 +0800

    新增Floating Button

--- app/src/main/java/com/example/york/teamcraft/teammanage/GroupManageFragment.java
+++ app/src/main/java/com/example/york/teamcraft/teammanage/GroupManageFragment.java
@@ -2,10 +2,12 @@ package com.example.york.teamcraft.teammanage;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.york.teamcraft.R;
 
@@ -17,13 +19,23 @@ public class GroupManageFragment extends Fragment {
 
     private static final String TAG = "GroupManageFragment";
 
+    private FloatingActionButton fab;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.team_groupmanage_fragment,container,false);
+        View view=inflater.inflate(R.layout.team_groupmanage_fragment, container, false);
 
+        initFab(view);
         return view;
     }
 
-
+    private void initFab(View view){
+        fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Toast.makeText(getActivity(), "FAB Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
