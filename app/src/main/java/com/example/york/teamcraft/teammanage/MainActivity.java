4cbb8ea967a026abce8d7e20d83c3e8c8bba29ff
York <kmgs4524@gmail.com>
2017-09-05 17:42:13 +0800

    將Drawer替換成NavigationView

--- app/src/main/java/com/example/york/teamcraft/teammanage/MainActivity.java
+++ app/src/main/java/com/example/york/teamcraft/teammanage/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.york.teamcraft.teammanage;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.york.teamcraft.R;
 import com.example.york.teamcraft.SignInActivity;
@@ -27,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
     /*----- Drawer相關元件 -------*/
     private String[] planetTitles;  //用來初始化navigation list的string array
     private DrawerLayout drawerLayout;
-    private ListView drawerList;
+    private NavigationView navigationView;
+//    private ListView drawerList;
     /*----------------------------*/
 
     @Override
@@ -71,34 +76,66 @@ public class MainActivity extends AppCompatActivity {
     private void initDrawer(){
         planetTitles = getResources().getStringArray(R.array.planets_array);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerList = (ListView) findViewById(R.id.left_drawer);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+//        drawerList = (ListView) findViewById(R.id.left_drawer);
+        Log.d(TAG, navigationView.toString());
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Toast.makeText(getApplicationContext(), "轉至個人管理", Toast.LENGTH_SHORT);
+                // 先檢查點擊的item是否為checked
+                if(item.isChecked()) {
+                    item.setChecked(false);
+                } else {
+                    item.setChecked(true);
+                }
 
-        //為drawerList設置adapter
-        drawerList.setAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, planetTitles));
-        //設置drawerList的listener
-        drawerList.setOnItemClickListener(new MainActivity.DrawerItemClickListener());
-    }
+                drawerLayout.closeDrawers();    //關閉drawer
+                Intent intent;
 
-    //AdapterView.OnItemClickListener
-    private class DrawerItemClickListener implements android.widget.AdapterView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
-
-        /** Swaps fragments in the main content view */
-        private void selectItem(int position) {
-            if(position == 0){
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, com.example.york.teamcraft.personalsmanage.MainActivity.class);
-                startActivity(intent);
-            } else if(position == 1){
-//                Intent intent = new Intent();
-//                intent.setClass(MainActivity.this, com.example.york.teamcraft.teammanage.MainActivity.class);
-//                startActivity(intent);
+                switch (item.getItemId()) {
+                    case R.id.drawer_personals:
+                        intent = new Intent();
+                        intent.setClass(MainActivity.this, com.example.york.teamcraft.personalsmanage.MainActivity.class);
+                        startActivity(intent);
+                        return  true;
+                    case R.id.drawer_team:
+                        intent = new Intent();
+                        intent.setClass(MainActivity.this, com.example.york.teamcraft.teammanage.MainActivity.class);
+                        startActivity(intent);
+                        return  true;
+                    case R.id.drawer_account:
+                        Toast.makeText(getApplicationContext(), "轉至帳戶資料", Toast.LENGTH_SHORT);
+                        return  true;
+                    default:
+                        Toast.makeText(getApplicationContext(), "Something Error", Toast.LENGTH_SHORT);
+                        return  true;
+                }
             }
+        });
 
-        }
     }
+
+// 下列註解區塊為舊的listView用法
+    //AdapterView.OnItemClickListener
+//    private class DrawerItemClickListener implements android.widget.AdapterView.OnItemClickListener {
+//        @Override
+//        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            selectItem(position);
+//        }
+//
+//        /** Swaps fragments in the main content view */
+//        private void selectItem(int position) {
+//            if(position == 0){
+//                Intent intent = new Intent();
+//                intent.setClass(MainActivity.this, com.example.york.teamcraft.personalsmanage.MainActivity.class);
+//                startActivity(intent);
+//            } else if(position == 1){
+////                Intent intent = new Intent();
+////                intent.setClass(MainActivity.this, com.example.york.teamcraft.teammanage.MainActivity.class);
+////                startActivity(intent);
+//            }
+//
+//        }
+//    }
 }
