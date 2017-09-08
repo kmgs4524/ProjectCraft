4cc3ac4c5124cc8930f05449efd66b308f1491b9
York <kmgs4524@gmail.com>
2017-09-08 16:45:32 +0800

    完成AddItemActivity中edtText的Getter，並新增NoteDBHelper, NotesContract

--- app/src/main/java/com/example/york/teamcraft/personalsmanage/AddItemActivity.java
+++ app/src/main/java/com/example/york/teamcraft/personalsmanage/AddItemActivity.java
@@ -1,24 +1,29 @@
 package com.example.york.teamcraft.personalsmanage;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
 import com.example.york.teamcraft.R;
 
 public class AddItemActivity extends AppCompatActivity {
+    private static String TAG = "AddItemActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personals_activity_add_item);
         initToolBar();
-        initEdtText();
+        initEdtBtn();
+
+
     }
 
     // 設置ToolBar
@@ -39,13 +44,22 @@ public class AddItemActivity extends AppCompatActivity {
         return true;
     }
 
-    public void initEdtText() {
-        EditText edtTitle = (EditText) findViewById(R.id.edt_item_title);
-        EditText Content = (EditText) findViewById(R.id.edt_item_content);
-        EditText edtDate = (EditText) findViewById(R.id.edt_item_date);
-        EditText edtTime = (EditText) findViewById(R.id.edt_item_time);
+    public void initEdtBtn() {
+        final EditText edtTitle = (EditText) findViewById(R.id.edt_item_title);
+        final EditText edtContent = (EditText) findViewById(R.id.edt_item_content);
+        final EditText edtDate = (EditText) findViewById(R.id.edt_item_date);
+        final EditText edtTime = (EditText) findViewById(R.id.edt_item_time);
+        Button btnCreate = (Button) findViewById(R.id.btn_create);
 
-
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("edtTitle",  edtTitle.getText().toString());
+                Log.d("edtContent", edtContent.getText().toString());
+                Log.d("edtDate", edtDate.getText().toString());
+                Log.d("edtTime", edtTime.getText().toString());
+            }
+        });
     }
 
     // 點擊重要性選項按鈕回傳對應的值
@@ -57,12 +71,15 @@ public class AddItemActivity extends AppCompatActivity {
         switch(view.getId()) {
             case R.id.radio_urgent: //急迫
                 if (checked)
+                    Log.d("radio_urgent", view.toString());
                     return 1;
             case R.id.radio_normal: //適中
                 if (checked)
+                    Log.d("radio_normal", view.toString());
                     return 2;
             case R.id.radio_ample:  //充裕
                 if (checked)
+                    Log.d("radio_ample", view.toString());
                     return 3;
             default:
                 return 0;
---
+++ app/src/main/java/com/example/york/teamcraft/personalsmanage/NotesContract.java
@@ -0,0 +1,8 @@
+package com.example.york.teamcraft.personalsmanage;
+
+/**
+ * Created by York on 2017/9/8.
+ */
+
+public final class NotesContract {
+}
