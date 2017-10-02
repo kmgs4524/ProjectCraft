4ae4f31a20fc36a47ac5ab89fb3dff1d317c8d3f
York <kmgs4524@gmail.com>
2017-10-02 09:15:28 +0800

    Work Class實作Parcelable類別

--- app/src/main/java/com/example/york/teamcraft/Work.java
+++ app/src/main/java/com/example/york/teamcraft/Work.java
@@ -1,10 +1,13 @@
 package com.example.york.teamcraft;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by York on 2017/9/28.
  */
 
-public class Work {
+public class Work implements Parcelable{
     private String topic;
     private String date;
     private String content;
@@ -17,6 +20,12 @@ public class Work {
 
     public Work() {}
 
+    protected Work(Parcel in) {
+        topic = in.readString();
+        date = in.readString();
+        content = in.readString();
+    }
+
     public String getTopic() {
         return topic;
     }
@@ -27,5 +36,29 @@ public class Work {
 
     public String getContent() {
         return content;
+    }
+
+    public static final Creator<Work> CREATOR = new Creator<Work>() {
+        @Override
+        public Work createFromParcel(Parcel in) {
+            return new Work(in);
+        }
+
+        @Override
+        public Work[] newArray(int size) {
+            return new Work[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(topic);
+        dest.writeString(date);
+        dest.writeString(content);
     }
 }
