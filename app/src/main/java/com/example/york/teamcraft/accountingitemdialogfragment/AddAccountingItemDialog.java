26d900a2cba8fbf9ce7bbdba07b79df5f31ab604
York <kmgs4524@gmail.com>
2017-11-07 15:38:34 +0800

    完成新增品項畫面二版

--- app/src/main/java/com/example/york/teamcraft/accountingitemdialogfragment/AddAccountingItemDialog.java
+++ app/src/main/java/com/example/york/teamcraft/accountingitemdialogfragment/AddAccountingItemDialog.java
@@ -7,8 +7,10 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.york.teamcraft.R;
@@ -32,21 +34,9 @@ public class AddAccountingItemDialog extends DialogFragment{
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_add_accounting_item, null);
         ButterKnife.bind(this, view);
-        builder.setView(view)
-                .setPositiveButton("確認", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                })
-                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
+        AlertDialog dialog =builder.setView(view)
+                .create();
         setAccountingTypeBtnColor();
-        Dialog dialog = builder.create();
         // 將Dialog的背景色改為透明
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
